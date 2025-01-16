20751f8ee2049429eec00c2c9f7bd5281a6f4a69
Rohan Bhangale <rohanbhangale25@gmail.com>
2025-01-16 18:55:49 +0530

    some minor fixes

--- backend/src/lib/util.js
+++ backend/src/lib/util.js
@@ -6,7 +6,7 @@ export const generateToken = (userId , res) => {
     });
 
     res.cookie("jwt" , token ,{
-        maxAge  : 7 * 24 * 60 * 60 * 1000,
+        maxAge  : 24 * 60 * 60 * 1000,
         httpOnly : true, // just for extra security purpose or to prevent xss attacks
         sameSite : "strict", // to prevent csrf attacks on the web or to prevent cross     site attacks
         secure   : process.env.NODE_ENV !== "development",
